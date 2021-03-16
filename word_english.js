8f472d2463c9fc1aaba1ec29c76b5353711d1349
SIMPLON <tellai>
2021-03-16 16:10:29 +0100

    fonction hello

--- word_english.js
+++ word_english.js
@@ -8,15 +8,20 @@ const json = require("./json_word/wordlist_byPOS.json");
 const obj = JSON.parse(JSON.stringify(json));
 
 const list_word = [];
+const ponct = " ==> ";
+let count = 1;
+
 for (let i = 0; i < 10; i++) {
   let clé = Object.keys(obj)[
     Math.floor(Math.random() * Object.keys(obj).length)
   ];
-  let word = obj[clé][Math.floor(Math.random() * obj[clé].length)];
+  let prefix = ((count).toString()).concat("", ponct)
+  let word = prefix.concat(obj[clé][Math.floor(Math.random() * obj[clé].length)]);
   list_word.push(word);
+  count++;
 }
 
-myclient.once('ready', () => {
+myclient.on('ready', client => {
     console.log(`Login as ${myclient.user.tag}`);
 });
 
@@ -24,6 +29,10 @@ myclient.on("message", (message) => {
     if (message.content === 'english'){
         message.channel.send(list_word);
     }
+    else if (message.content === 'hello'){
+      message.channel.send("Salut !")
+    }
 })
+
 
 myclient.login(token);
