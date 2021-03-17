b559290afdcc73b924e839252e4801cad1f1c4c0
MarriaMarria <maria.kononevs@gmail.com>
2021-03-17 21:18:53 +0100

    saving files

---
+++ test/hello_maria_test.js
@@ -0,0 +1,4 @@
+// const weather = require('..test/weather_maria.js').weather;
+// console.log(weather)
+
+// connection
--- weather_maria.js
+++ weather_maria.js
@@ -1,18 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// I make a function for each small function
 client.on('ready', () => {
     console.log(`${client.user.tag}! is here and ready to yell!`);
-});
+}); // function which checks if I am connected
+
 
 
 const weather = require("weather-js");
+console.log(typeof weather) // object
+
+// to check if the library still exists, still working the same way is before
 
 weather.find(
     { search: "Paris, France", degreeType: "C" },
     function (err, result) {
     if (err) console.log(err);
 
+    // check if my phrase has keys I need, if there is an input, it is not empty
+    // ex if I put METEO, I must receive an answer
     client.on("message", (msg) => {
         if (msg.content === "meteo") {
             msg.channel.send(fullMeteo);
@@ -20,6 +27,7 @@ weather.find(
         }
     });
 
+    // test that json in result has the keys that I need
 
     const weatherFull = result[0]
     const location = weatherFull["location"]
