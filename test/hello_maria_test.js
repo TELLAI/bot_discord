42f8f67e3f33920051459c6654a9b79634488c62
MarriaMarria <maria.kononevs@gmail.com>
2021-03-17 22:26:51 +0100

    changed weather_maria,divided the code in 3 functions, imported them to test_file

--- test/hello_maria_test.js
+++ test/hello_maria_test.js
@@ -1,4 +1,9 @@
-// const weather = require('..test/weather_maria.js').weather;
-// console.log(weather)
+const assert = require('chai').assert;
+const weather_maria = require('../weather_maria');
+
+const connection = require('../weather_maria.js').connection;
+const response = require('../weather_maria.js').response;
+const findWeather = require('../weather_maria.js').findWeather;
 
 // connection
+
--- weather_maria.js
+++ weather_maria.js
@@ -1,51 +1,67 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client();   
 
-// I make a function for each small function
-client.on('ready', () => {
-    console.log(`${client.user.tag}! is here and ready to yell!`);
-}); // function which checks if I am connected
+function connection() {
+    // I make a function for each small function
+    client.on('ready', () => {
+        console.log(`${client.user.tag}! is here and ready to yell!`);
+    }); // function which checks if I am connected
 
-
+}   
+connection()
 
 const weather = require("weather-js");
-console.log(typeof weather) // object
+// console.log(typeof weather) // object
 
 // to check if the library still exists, still working the same way is before
 
-weather.find(
-    { search: "Paris, France", degreeType: "C" },
-    function (err, result) {
-    if (err) console.log(err);
+const details = {
+    search: "Paris, France", 
+    degreeType: "C",
+}
 
-    // check if my phrase has keys I need, if there is an input, it is not empty
-    // ex if I put METEO, I must receive an answer
+function response(fullMeteo) {
     client.on("message", (msg) => {
         if (msg.content === "meteo") {
             msg.channel.send(fullMeteo);
             // or reply instead of channel.send
         }
     });
+}
 
-    // test that json in result has the keys that I need
-
-    const weatherFull = result[0]
-    const location = weatherFull["location"]
-    const paris = location["name"]
-    const weatherInfo = weatherFull["current"] // temp, wind, feelslike etc
-    const temp = weatherInfo["temperature"]
-    const wind = weatherInfo["windspeed"]
-    const feels = weatherInfo["feelslike"]
-    const sky = weatherInfo["skytext"]
-    const humidity = weatherInfo["humidity"]
+function findWeather() {
+    weather.find(
+        details,
+        function (err, result) {
+        if (err) console.log(err);
+        // console.log(result)
+        // check if my phrase has keys I need, if there is an input, it is not empty
+        // ex if I put METEO, I must receive an answer
     
-    const fullMeteo = `Weather in ${paris} today: ${sky}, temperature is ${temp}°C, real feeling is ${feels}°C, humidity level is ${humidity} and the speed of wind is ${wind} km/h. Have a lovely day!`
-    // console.log(fullMeteo)// const temperature = 
-    // console.log(result[0]["location"]["name"]);
-    // console.log(result[0]["current"]["temperature"]);
-    // console.log(result[0]["forecast"][0]['day']);
-    }
-);
+        // test that json in result has the keys that I need
+    
+        const weatherFull = result[0]
+        const location = weatherFull["location"]
+        const paris = location["name"]
+        const weatherInfo = weatherFull["current"] // temp, wind, feelslike etc
+        const temp = weatherInfo["temperature"]
+        const wind = weatherInfo["windspeed"]
+        const feels = weatherInfo["feelslike"]
+        const sky = weatherInfo["skytext"]
+        const humidity = weatherInfo["humidity"]
+        
+        const fullMeteo = `Weather in ${paris} today: ${sky}, temperature is ${temp}°C, real feeling is ${feels}°C, humidity level is ${humidity} and the speed of wind is ${wind} km/h. Have a lovely day!`
+        response(fullMeteo);
+        // console.log(fullMeteo)// const temperature = 
+        // console.log(result[0]["location"]["name"]);
+        // console.log(result[0]["current"]["temperature"]);
+        // console.log(result[0]["forecast"][0]['day']);
+        }
+    );
+    
+}
+
+findWeather()
 
 const { token } = require("./config.json");
 client.login(token);
