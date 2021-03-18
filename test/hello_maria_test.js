5a3f991ea85865274e56df575b496a58f2fad66e
MarriaMarria <maria.kononevs@gmail.com>
2021-03-18 15:03:34 +0100

    save

--- test/hello_maria_test.js
+++ test/hello_maria_test.js
@@ -1,21 +1,18 @@
 const assert = require('chai').assert;
-const weather_maria = require('../weather_maria');
+// const assert = require("assert");
+const importFile = require('../functions_weather.js');
 
-const connection = require('../weather_maria.js').connection;
-const response = require('../weather_maria.js').response;
-const findWeather = require('../weather_maria.js').findWeather;
-const parseResult = require('../weather_maria.js').parseResult;
-// import { parseResult } from '../weather_maria.js';
-// response
 
 describe('testing weather file', function() {
     it('check if fullMeteo is a string datatype', function() {
-        let result = parseResult();
-        assert.typeOf(result, 'string')
-    })
-}
+        assert.equal(importFile.parsing_json, 'string')
+    }),
 
-// })
+    it("test response", function () {
+        assert.equal(importFile.response("meteo"), fullMeteo);
+    });
+
+})
 
 
 // describe('testing weather file', function() {
