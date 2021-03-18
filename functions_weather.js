6e3cf26cbc23723db621aab632be719a33d81305
MarriaMarria <maria.kononevs@gmail.com>
2021-03-18 23:18:41 +0100

    save

--- functions_weather.js
+++ functions_weather.js
@@ -1,87 +1,61 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();   
-client.on('ready', () => {
-    console.log(`${client.user.tag}! is here and ready to yell!`);
-});
 const weather = require("weather-js");
+
 const details = {
     search: "Paris, France", 
     degreeType: "C",
 }
 
-module.exports = {
+// second hand functions which I don't export
+
+function getWeather() { // promise is used to wait for smth 
+    return new Promise((resolve, reject) => { // copy paste , resolve = success, in our case we get a string
+        weather.find(details, async (err, result) => {
+            let weatherParis = parseResult(err, result);
+            // console.log(weatherParis + "WEATHER PARIS")
+            if(weatherParis) resolve(weatherParis); // if we got a string we succeeded 
+            else reject();
+    });
+});
+
+}
+
+function parseResult(err, result)  {
+    if (err) console.log(err);
+
+    const weatherFull = result[0]
+    const location = weatherFull["location"]
+    const paris = location["name"]
+    const weatherInfo = weatherFull["current"] // temp, wind, feelslike etc
+    const temp = weatherInfo["temperature"]
+    const wind = weatherInfo["windspeed"]
+    const feels = weatherInfo["feelslike"]
+    const sky = weatherInfo["skytext"]
+    const humidity = weatherInfo["humidity"]
     
-    response: function(fullMeteo) {
-        client.on("message", (msg) => {
-            if (msg.content === "meteo") {
-                msg.channel.send(fullMeteo);
-                // or reply instead of channel.send
-            }
-        });
-    },
-
-    parsing_json: parseResult = (err, result) => {
-        if (err) console.log(err);
-
-        const weatherFull = result[0]
-        const location = weatherFull["location"]
-        const paris = location["name"]
-        const weatherInfo = weatherFull["current"] // temp, wind, feelslike etc
-        const temp = weatherInfo["temperature"]
-        const wind = weatherInfo["windspeed"]
-        const feels = weatherInfo["feelslike"]
-        const sky = weatherInfo["skytext"]
-        const humidity = weatherInfo["humidity"]
-        
-        const fullMeteo = `Weather in ${paris} today: ${sky}, temperature is ${temp}°C, real feeling is ${feels}°C, humidity level is ${humidity} and the speed of wind is ${wind} km/h. Have a lovely day!`
-        response(fullMeteo);
-        // console.log(fullMeteo)
-        // console.log(typeof fullMeteo)
-        return fullMeteo;
-        },
-
-    get_weather: function findWeather() {
-        weather.find(details, (err,result) => parseResult(err, result) 
-        );
-
+    const fullMeteo = `Weather in ${paris} today: ${sky}, temperature is ${temp}°C, real feeling is ${feels}°C, humidity level is ${humidity} and the speed of wind is ${wind} km/h. Have a lovely day!`
+    console.log(fullMeteo);
+    return fullMeteo;
     }
 
 
+module.exports = {
+// function I export to use in index.js
+
+    searchAndReply: async function (msg) {
+        try {
+            if (msg.content === "meteo") { // I need msg and I must have async to wait for the reply from the library
+                let fullMeteo = await getWeather(); 
+                msg.channel.send(fullMeteo);
+                // or reply instead of channel.send
+            }
+        } catch(err) {
+            console.log("message error: ", err)
+        }
+    }
 
 }
-const { token } = require("./config.json");
-client.login(token);
-
-// module.exports = {
-//     parseResult
-// }
 
 
 
 
 
-
-
-
-
-
-
-
-// export function Course() {
-//     this.id = '';
-//     this.name = '';
-// };
-// import { Course } from './course.js';
-
-
-
-
-
-// function connection() {
-//     // I make a function for each small function
-//     client.on('ready', () => {
-//         console.log(`${client.user.tag}! is here and ready to yell!`);
-//     }); // function which checks if I am connected
-
-// }   
-// connection()
--- weather_maria.js
+++ weather_maria.js
@@ -1,20 +1,22 @@
-// const Discord = require('discord.js');
-// const client = new Discord.Client();  
-// const { token } = require("./config.json");
-// client.login(token);
+// configuration
+// which functions use when 
+const Discord = require('discord.js');
+const client = new Discord.Client();  
+const { token } = require("./config.json");
+const importModule = require("./functions.js")
+// import searchAndReply from "./functions.js"; //??
 
 
-// client.on("message", (msg) => {
-//     if (msg.content === "!meteo") {
-//         msg.channel.send(fullMeteo);
-//         // or reply instead of channel.send
-//     }
-// });
+client.on('ready', () => {
+    console.log(`${client.user.tag}! is here and ready to yell!`);
+}); 
 
-const importFile = require('./functions_weather.js');
 
-const { response } = require('./functions_weather.js');
+client.on("message", (msg) => importModule.searchAndReply(msg));
+// (msg) => don't execute now, wait for smth (msg) 
 
-importFile.parseResult
-importFile.findWeather
-importFile.response
+
+
+
+
+client.login(token);
